6f6873c370d301c3f8b8aecbc6392c9c0b11a216
Yunus <yunusemretuncer111@email.com>
2026-03-10 14:17:22 +0300

    Ignore infrastructure folder

--- infrastructure/modules/security/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID where security groups will be created"
-  type        = string
-}
-
-variable "environment" {
-  description = "Deployment environment (dev, prod, etc.)"
-  type        = string
-}
