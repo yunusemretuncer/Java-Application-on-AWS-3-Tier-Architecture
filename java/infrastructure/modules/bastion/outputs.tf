8152468da5e3b38ac948ee0dab83954889f73c4d
yunusemretuncer <yunusemretuncer111@gmail.com>
2026-03-10 15:03:38 +0300

    Delete java/infrastructure directory

--- java/infrastructure/modules/bastion/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip"     { value = aws_instance.bastion.public_ip }
-output "instance_id"   { value = aws_instance.bastion.id }
-output "bastion_sg_id" { value = aws_security_group.bastion_sg.id }
--- java/infrastructure/modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "endpoint" {
-  description = "RDS endpoint"
-  value       = aws_db_instance.this.address
-}
-
-output "db_name" {
-  description = "RDS database name"
-  value       = aws_db_instance.this.db_name
-}
